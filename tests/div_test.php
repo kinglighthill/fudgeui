dc7f1d824de36902dbe125b7dbef2e78784ff065
Francis-PC\Francis <francis94c@gmail.com>
2016-11-11 16:27:55 +0100

    div class testing adjustments

--- tests/div_test.php
+++ tests/div_test.php
@@ -1,9 +1,22 @@
 <?php
-class HTML5Test extends PHPunit_Framework_Testcase {
-    function testDIVConstructor() {
-        $div = new DIV("id");
-        $this->assertEquals("id", $div->getID());
-        $this->assertEquals("div", $div->getTagName());
-    }
+class DIVTest extends PHPunit_Framework_Testcase {
+  private $logger;
+  function __construct() {
+    $this->logger = new VLogger("test_logs.txt");
+  }
+  function testDIVConstructor() {
+    $div = new DIV("id");
+    $this->assertEquals("id", $div->getID());
+    $this->assertEquals("div", $div->getTagName());
+  }
+  function testDIVAttributesFormatting() {
+    $div = new DIV("id");
+    $result = strpos($div->getView(), "<div id=\"id\">" . PHP_EOL . "</div>") != false;
+    $this->logger->log($div->getView());
+    $this->assertTrue($result);
+    $div->addClass("hello");
+    $result = strpos($div->getView(), "<div id=\"id\" class=\"hello\">" . PHP_EOL . "</div>") != false;
+    $this->assertTrue($result);
+  }
 }
 ?>
