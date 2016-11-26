59e22a7503ba30cb9571be17743588bf612066be
Francis-PC\Francis <francis94c@gmail.com>
2016-11-27 00:20:02 +0100

    created an Exception for invalid input types

--- lib/Exceptions.php
+++ lib/Exceptions.php
@@ -1,3 +1,8 @@
 <?php
- 
+class InvalidInputTypeException extends Exception {
+  public function message() {
+    $errorMsg = "Invalid input type given on line " . $this->getLine() . " in " . $this->getFile() . "; <b>" . $this->getMessage() . "</b>";
+    return $errorMsg;
+  }
+}
 ?>
