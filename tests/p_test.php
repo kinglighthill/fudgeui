d3262ab6cd30be80310ffa8fb2a44a34af856685
Francis-PC\Francis <francis94c@gmail.com>
2016-11-28 06:25:16 +0100

    overloaded constructor of P object was tested

--- tests/p_test.php
+++ tests/p_test.php
@@ -4,6 +4,12 @@ class PTest extends PHPunit_Framework_Testcase {
   function __construct() {
     $this->logger = new VLogger("test_logs.txt");
   }
+  function testPConstructor() {
+    $p = new P("Hello World");
+    $this->assertEquals(1, preg_match("/<p>Hello World<\/p>/", $p->getView()));
+    $p = new P("id3", "Hello World");
+    $this->assertEquals(1, preg_match("/<p id=\"id3\">Hello World<\/p>/", $p->getView()));
+  }
   function testPBody() {
     $p = new P("Hello Madagascar");
     $this->logger->log($p->getView());
